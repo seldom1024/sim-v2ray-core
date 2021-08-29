c5a518475be2765eed973831046eb1286a244cba
seldo <zhangqipaq@163.com>
2021-08-29 19:26:26 +0800

    init

---
+++ io/jsonvconfigmarshaller.go
@@ -0,0 +1,32 @@
+package io
+
+import (
+	"encoding/json"
+	core "github.com/seldom1024/sim-v2ray-core"
+)
+
+type JsonVUser struct {
+	id    string `json:"id"`
+	email string `json:"email"`
+}
+
+type JsonVConfig struct {
+	RunAs    string      `json:"runas"`
+	Port     uint8       `json:"port"`
+	Clients  []JsonVUser `json:"users"`
+	Protocol string      `json:"protocol"`
+}
+
+type JsonVConfigUnmarshaller struct {
+}
+
+func (*JsonVConfigUnmarshaller) Unmarshall(data []byte) (*core.VConfig, error) {
+	var jsonConfig JsonVConfig
+	err := json.Unmarshal(data, &jsonConfig)
+	if err != nil {
+		return nil, err
+	}
+	var vconfig = new(core.VConfig)
+	vconfig.RunAs = core.VUser{}
+	return vconfig, nil
+}
---
+++ net/vmesshandler.go
@@ -0,0 +1,8 @@
+package net
+
+type VMessHandler struct {
+}
+
+func (*VMessHandler) Listen(port uint8) error {
+	return nil
+}
