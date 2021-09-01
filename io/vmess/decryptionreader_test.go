5a7d2fbbafe82efed70c0916316aeccb990f150a
seldo <zhangqipaq@163.com>
2021-09-01 22:31:23 +0800

    general purpose encryption streams V2Ray

--- io/vmess/decryptionreader_test.go
+++ io/vmess/decryptionreader_test.go
@@ -3,6 +3,7 @@ package vmess
 import (
 	"bytes"
 	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	mrand "math/rand"
 	"testing"
@@ -26,33 +27,34 @@ func TestNormalReading(t *testing.T) {
 	keySize := 16
 	key := make([]byte, keySize)
 	randomBytes(key, t)
+	iv := make([]byte, keySize)
+	randomBytes(iv, t)
 
-	cipher, err := aes.NewCipher(key)
+	aesBlock, err := aes.NewCipher(key)
 	if err != nil {
 		t.Fatal(err)
 	}
+	aesMode := cipher.NewCBCEncrypter(aesBlock, iv)
 
 	ciphertext := make([]byte, testSize)
-	for encryptSize := 0; encryptSize < testSize; encryptSize += blockSize {
-		cipher.Encrypt(ciphertext[encryptSize:], plaintext[encryptSize:])
-	}
+	aesMode.CryptBlocks(ciphertext, plaintext)
 
-	ciphertextCopy := make([]byte, testSize)
-	copy(ciphertextCopy, ciphertext)
+	ciphertextcopy := make([]byte, testSize)
+	copy(ciphertextcopy, ciphertext)
 
-	reader, err := NewDecryptionReader(bytes.NewReader(ciphertextCopy), key)
+	reader, err := NewDecryptionReader(bytes.NewReader(ciphertextcopy), key, iv)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	readText := make([]byte, testSize)
+	readtext := make([]byte, testSize)
 	readSize := 0
 	for readSize < testSize {
 		nBytes := mrand.Intn(16) + 1
 		if nBytes > testSize-readSize {
 			nBytes = testSize - readSize
 		}
-		bytesRead, err := reader.Read(readText[readSize : readSize+nBytes])
+		bytesRead, err := reader.Read(readtext[readSize : readSize+nBytes])
 		if err != nil {
 			t.Fatal(err)
 		}
@@ -61,7 +63,7 @@ func TestNormalReading(t *testing.T) {
 		}
 		readSize += nBytes
 	}
-	if !bytes.Equal(readText, plaintext) {
-		t.Errorf("Expected plaintext %v, but got %v", plaintext, readText)
+	if !bytes.Equal(readtext, plaintext) {
+		t.Errorf("Expected plaintext %v, but got %v", plaintext, readtext)
 	}
 }
